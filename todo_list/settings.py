a38784b80e0b58c552f48daba1751b726ce4c8c1
Kuldeep <kskiafoo7@gmail.com>
2022-04-17 05:04:39 +0530

    backend complete

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
